3efd90762c78d780f387f93f8df5b34edc3efa55
slinder8 <sjtlinder@icloud.com>
2026-01-23 11:09:41 -0500

    100 200

--- breakwater/src/bw_config.h
+++ breakwater/src/bw_config.h
@@ -19,9 +19,9 @@
 */
 
 /* delay threshold to detect congestion */
-#define SBW_DELAY_TARGET		150
+#define SBW_DELAY_TARGET		100
 /* delay threshold for AQM */
-#define SBW_DROP_THRESH			150
+#define SBW_DROP_THRESH			200
 
 /* round trip time in us */
 #define SBW_RTT_US			10
